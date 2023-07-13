59e8110fda077dd3ffb6b86c013ecd9c1da64ee2
Ihor Bobulych <ihbobuacc@gmail.com>
2023-07-13 14:53:34 +0300

    Extract jobs and companies data from the server and display on the client side

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,7 +1,18 @@
-import {getJobs} from "./db/jobs.js";
+import {getJob, getJobs} from "./db/jobs.js";
+import {getCompany} from "./db/companies.js";
 
 export const resolvers = {
     Query: {
         jobs: () => getJobs(),
+        job: (_root, {id}) => getJob(id),
+        company: (_root, {id}) => getCompany(id),
+    },
+    Job: {
+        date: (job) => toIsoDate(job.createdAt),
+        company: (job) => getCompany(job.companyId)
     },
 };
+
+function toIsoDate(value) {
+    return value.slice(0, 'yyyy-mm-dd'.length);
+}
