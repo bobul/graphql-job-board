0446796645688ceb9d67f13f52780b2b0a5d3123
Ihor Bobulych <ihbobuacc@gmail.com>
2023-07-15 09:50:32 +0300

    Throw custom GraphQL errors

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,11 +1,23 @@
 import {getJob, getJobs, getJobsByCompany} from "./db/jobs.js";
 import {getCompany} from "./db/companies.js";
+import {GraphQLError} from "graphql/error/index.js";
 
 export const resolvers = {
     Query: {
         jobs: () => getJobs(),
-        job: (_root, {id}) => getJob(id),
-        company: (_root, {id}) => getCompany(id),
+        job: async (_root, {id}) => {
+            const job = await getJob(id);
+            if (!job){
+                throw notFoundError('No job found with ID: ' + id);
+            }
+        },
+        company: async (_root, {id}) => {
+            const company = await getCompany(id);
+            if (!company) {
+                throw notFoundError('No company found with ID: ' + id);
+            }
+            return company;
+        },
     },
     Company: {
         jobs: (company) => getJobsByCompany(company.id),
@@ -18,4 +30,12 @@ export const resolvers = {
 
 function toIsoDate(value) {
     return value.slice(0, 'yyyy-mm-dd'.length);
+}
+
+function notFoundError(message){
+    return new GraphQLError(message, {
+        extensions: {
+            code: 'NOT_FOUND',
+        }
+    });
 }
